41d600752109744c7130672a1913a162ab443b43
JordanTheDodger <54824287+JordanTheDodger@users.noreply.github.com>
2020-01-11 10:43:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,93 +1,88 @@
-const yargs = require('yargs')
+const fs = require('fs')
 const chalk = require('chalk')
-const notes = require('./notes.js')
 
 
-//Create a add command
-yargs.command({
-    command:'add',
-    describe:'Adding a new note',
-    builder:{
-        title:{
-            describe: 'Note Title',
-            demandOption: true, // if it is true then you to provide title to work command properly
-            type:'string'
-        },
-    //Challenge 5    
-        body:{
-            describe:'Note body',
-            demandOption:true,
-            type:'string'
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateSingleNote = notes.find((note) => notes.title === title)
+    //retunrs undefined if no note is found
 
-        }
-    //end Cahllenge 5
-    },
-    handler: (argv) => {
-        notes.addNote(argv.title, argv.body)
-        console.log('Title:- ' + argv.title, 
-                     '\nBody:- ' + argv.body)
+    if (duplicateSingleNote.length === undefined /* !duplicateSingleNote */) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.greenBright.inverse('New Note Added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken'))
     }
-})
+    
+}
 
-yargs.command({
-    command:'remove',
-    describe:'Removing a note',
-    builder:{
-        title:{
-            describe:'Note Title',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler: (argv) => {
-        notes.removeNote(argv.title)
-        console.log(argv.title + 'note is removed')
+//Challenge 6
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const noteToKeep = notes.filter( (singleNote) => {
+        return singleNote.title !== title;
+    })
+    const noteMatch = notes.filter((singleNote) => singleNote.title === title)
+    
+    if (noteMatch.length === 0){
+         console.log(chalk.bgRed('Note not found'))        
+     }else {
+         console.log(chalk.bgGreen('Note Removed'))
+         saveNotes(noteToKeep)
+ 
+    console.log('Updated Notes :- ') 
+    console.log(noteToKeep)
+    console.log('Removed Note')
+    console.log(noteMatch)
+}
+  
+    
+const listNotes = () => {
+    const notes = loadNotes()
+    console.log(chalk.inverse('Your notes'))
+    notes.forEach(element => {
+         console.log(element.title)
+     })
+}
+
+const readNote = (title) =>{
+    const notes = loadNotes()
+    const matchNote = notes.find((singleNote) => singleNote.title === title )
+    
+    if (matchNote) {
+        console.log(chalk.bgBlack.bold('TITLE:- '))
+        console.log(chalk.inverse(matchNote.title))
+        console.log(chalk.bgBlack.bold('DESCRIPTION:- '))
+        console.log(chalk.bgBlue(matchNote.body))
+    } else {
+        console.log(chalk.red.inverse('Note Not found'))
     }
-})
+}
 
-//Challenge 4
-yargs.command({
-    command:'read',
-    describe:'Reading a note',
-    handler: () => {
-        console.log('Reading a note!')
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+
+    } catch (e) {
+        return []
     }
-})
+    
+}
 
-yargs.command({
-    command:'list',
-    describe:'Listing notes',
-    handler: () => {
-        console.log('Here is list of notes!')
-    }
-})
-// end Challenge 4
-yargs.parse() // or console.log(yargs.argv) it does the same thing 
-
-
-
-
-
-
-
-/*Challenges
-console.log('Challenge 2:- ' + notes())
-console.log('Challenge 3:- ' + chalk.greenBright.bold('Success!'))
-console.log('Challenge 3:- ' + chalk.bgBlue('Success!'))
-*/
-
-//console.log(yargs.argv)
-
-/*const command = process.argv[2]
-if (command === 'add'){
-    console.log('Adding Notes!')
-}*/
-
-/*const validator = require('validator')
- console.log('Checking validator.isEmail :- ' + validator.isEmail('jordantheripper@gmail.com'))
- console.log('Checking validator.isURL :- ' + validator.isURL('abc.com'))
-*/
-
-/*const add = require('./utils.js')
-const sum = add(3,4)
-console.log(sum)*/
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
